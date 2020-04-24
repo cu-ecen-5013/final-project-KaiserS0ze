540bc6ebd278dc381b3b44e0806f92067c17dfae
KaiserS0ze <abhijeet.ds63@gmail.com>
2020-04-23 21:46:19 -0700

    opencv application dependencies

--- build.sh
+++ build.sh
@@ -16,11 +16,16 @@ IMAGE="IMAGE_FSTYPES = \"tar.bz2 ext3 wic.bz2 wic.bmap rpi-sdimg\""
 #Set GPU memory as minimum
 MEMORY="GPU_MEM = \"128\""
 #Add any packages needed here
-ADD_PACK="CORE_IMAGE_EXTRA_INSTALL += \"opencv libopencv-core-dev libopencv-highgui-dev libopencv-imgproc-dev libopencv-objdetect-dev libopencv-ml-dev opencv-dev opencv-apps mosquitto mosquitto-clients python3-paho-mqtt\""
+ADD_PACK="CORE_IMAGE_EXTRA_INSTALL += \"opencv libopencv-core-dev libopencv-highgui-dev libopencv-imgproc-dev \
+										libopencv-objdetect-dev libopencv-ml-dev opencv-dev opencv-apps \
+										mosquitto mosquitto-clients python3-paho-mqtt \
+										libswscale-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev \ 
+										libgtk2.0-dev libpng-dev libjpeg-dev libopenexr-dev libtiff-dev libwebp-dev imutils matplotlib \
+										argparse opencv-contrib-python scipy\""
 #Add wifi support
 DISTRO_F="DISTRO_FEATURES_append = \"wifi\""
 #add firmware support 
-IMAGE_ADD="IMAGE_INSTALL_append = \"linux-firmware-rpidistro-bcm43430 v4l-utils python3 cam-mqtt\""
+IMAGE_ADD="IMAGE_INSTALL_append = \"linux-firmware-rpidistro-bcm43430 v4l-utils python3 cam-mqtt python-numpy python-dev python3-numpy libav\""
 #linux-firmware-bcm43430
 #wpa-supplicant
 #kernel-module-brcmfmac bluez5 i2c-tools bridge-utils hostapd dhcp-server networkmanager iptables
@@ -137,6 +142,9 @@ layer_networking_info=$?
 bitbake-layers show-layers | grep "meta-camcam" > /dev/null
 layer_camcam_info=$?
 
+bitbake-layers show-layers | grep "meta-multimedia" > /dev/null
+layer_multimedia_info=$?
+
 #bitbake layers needed 
 
 if [ $layer_metaoe_info -ne 0 ];then
@@ -160,6 +168,14 @@ if [ $layer_networking_info -ne 0 ];then
 	bitbake-layers add-layer ../meta-openembedded/meta-networking
 else
 	echo "layer meta-networking already exists"
+fi
+
+
+if [ $layer_multimedia_info -ne 0 ];then
+    echo "Adding meta-multimedia layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+else
+	echo "layer meta-multimedia already exists"
 fi
 
 if [ $layer_info -ne 0 ];then
